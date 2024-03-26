c427194f2a9f35ca2dc641db0bf8ede9ffab7199
David <gomezd6@mcmaster.ca>
2024-03-26 19:00:21 -0400

    Added NavBar component and NextUI lib

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import MarketData from './components/MarketData'
+import NavBar from './components/NavBar'
+import EarningsReport from './components/EarningsReport'
 
 function index() {
   return (
     <div>
+      <NavBar/>
       <MarketData/>
+      <EarningsReport/>
     </div>
   )
 }
